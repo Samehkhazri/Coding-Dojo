be820d74feee7c58e7e9832db5a4539bdd5971df
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-17 16:33:09 +0100

    day 1 commit

---
+++ Programming Basics/Week 1/Day 1/Core/Khazri_sameh_JS_CreateVariables.js
@@ -0,0 +1,30 @@
+//level(1);
+
+//challange(1);
+var x=5;
+console.log(x);//5
+
+//challange(2);
+var x=3;
+x=x+1;
+console.log(x);//4
+
+//challange(3);
+var x =3;
+x=x+2;
+console.log(x+2);//7
+console.log(x);//5
+
+//challange(4);
+var x = 3;
+var x = 3*3;
+console.log(x);//9
+console.log(x*2);//18
+
+//challange(5);
+var x =2;
+var y =5;
+var z = x+y;
+console. log(z);//7
+
+
