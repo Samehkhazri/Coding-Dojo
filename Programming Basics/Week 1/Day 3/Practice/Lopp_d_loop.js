98ff5412fd4c482e3b7a7f826d1f6b46199a23f4
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-18 18:11:50 +0100

    Day3 Practice commit

---
+++ Programming Basics/Week 1/Day 3/Practice/Lopp_d_loop.js
@@ -0,0 +1,9 @@
+//when we need somthing to be repeated
+// starting point is 2 miles
+// the loop should stop in 6 miles
+// incrementing by 1
+// variable is i
+
+for(i=2;i<=6;i++){
+    console.log("candy")
+}
