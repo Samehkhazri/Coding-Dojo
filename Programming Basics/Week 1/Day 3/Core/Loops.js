115ccabdbbfb8c59b8028dce2d8108f9b117288c
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-19 10:53:10 +0100

    day3 commit

---
+++ Programming Basics/Week 1/Day 3/Core/Loops.js
@@ -0,0 +1,8 @@
+for(var year=1996;year<=2023;year++) {
+    console.log(year+" JS is awesome");
+
+    if ((year-1996) % 10 == 0 && (year != 1996)) {
+        console.log(year+" happy decade of JS!");
+    }
+}
+console.log("HAPPY 27 years of JAVASCRIPT!");
---
+++ Programming Basics/Week 1/Day 3/Practice/Khazri_Sameh_JS_Predict.js
@@ -0,0 +1,26 @@
+
+//Predict 1
+
+function myBirthYearFunc(){
+    console.log("I was born in" + 1980); //I was born in1980
+}
+myBirthYearFunc(1980);
+
+//predict 2
+
+function myBirthYearFunc(birthYearInput){
+    console.log("I was born in" + birthYearInput);// I was born in1980
+}
+myBirthYearFunc(1980);
+
+//Predict 3
+
+function add(num1, num2){
+    console.log("Summing Numbers!"); // Suming Numbers!
+    console.log("num1 is: " + num1); // num1 is 10
+    console.log("num2 is: " + num2); // num2 is 20
+    var sum = num1 + num2;
+    console.log(sum);  // 30
+}
+add(10, 20);
+
