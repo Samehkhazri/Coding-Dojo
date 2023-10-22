cb9a32880e8cb50ca6008693f854c6c862575790
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-22 11:11:04 +0100

    Pre-bootcamp

---
+++ Pre-bootcamp/Pridect 2.js
@@ -0,0 +1,26 @@
+// Code Snippet
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo); 
+}
+
+//["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+
+
+//Code Snippet 2
+function displayGrocerylist() {
+    var produce = ["apples", "oranges"];
+    var frozen = ["broccoli", "ice cream"];
+    frozen.push("hashbrowns");
+    console.log(frozen);
+}
+//["broccoli", "ice cream","hashbrowns"]
+
+//Code Snippet 3
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+//["Bambi", "Beetlejuice", "Toy Story","Zoro"]
+
