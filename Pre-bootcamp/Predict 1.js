ef2c946097beca1f62448ff5d7d6aa09345d6a89
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-22 10:38:11 +0100

    Pre-bootcamp

---
+++ Pre-bootcamp/Predict 1.js
@@ -0,0 +1,28 @@
+
+//Pridect 1
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);// I Was born in 1980
+
+    
+    //pridect 2
+    function myBirthYearFunc(birthYearInput){
+         console.log("I was born in " + birthYearInput); // I Was born in 1980
+        }
+        
+    }
+    
+  
+
+    //Pridect 3
+    function add(num1, num2){
+        num1=10
+        num2=30
+            console.log("Summing Numbers!");//Summing Numbers
+            console.log("num1 is: " + num1);//num1 is 10
+            console.log("num2 is: " + num2);//num2 is 20
+            var sum = num1 + num2;//10+30
+            console.log(sum);//30
+        }
+        
+        
+        
