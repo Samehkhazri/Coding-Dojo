977507450b9307215bc66b72879bdc75ca48bfd6
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-29 21:16:57 +0100

    Button Clicker

---
+++ WebFundamentals/WebFundamentals/Week 1/Day 5/Core/Button Clicker/script.js
@@ -0,0 +1,21 @@
+function login(element){
+    if (element.innerText == "login") {
+        element.innerText = "Logout";
+    }
+    else{
+        element.innerText = "login";
+    }
+}
+
+
+function remove(button){
+    button.remove();
+}
+
+
+
+
+function alerte(){
+    alert("This page says Ninja was liked");
+}
+
