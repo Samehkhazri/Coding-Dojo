626fff00e7039e69b96b1984db95efbb6acb099c
Samehkhazri <sameh.khazri09@gmail.com>
2023-10-30 14:07:44 +0100

    Loop challenges

---
+++ WebFundamentals/Week 1/Day 5/Practice/Loop Challenges.js
@@ -0,0 +1,33 @@
+//Print odds 1-20
+for(var i=1;i<=20;i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+}
+//Decreasing Multiples of 3
+//console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+for(var i=100;i>=0;i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+//Print the sequence
+for(var i=4;i>=-3.5;i-=1.5){
+    console.log(i);
+}
+//Sigma
+//Write code that will add all of the values from 1-100 onto some variable sum 
+var sum=0;
+for(var i=1;i<=100;i++){
+    sum=sum+i;
+    console.log(sum);
+    
+}
+//Factorial
+//Write code that will multiply all of the values from 1-12 onto some variable product
+var product[]
+for(var i=1;i<=12;i++){
+    product =product *i;
+    console.log(product );
+
+}
