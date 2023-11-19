7e9102229e15cd70ec6ca0c2dc0f148c9d2b038e
Samehkhazri <sameh.khazri09@gmail.com>
2023-11-19 13:09:35 +0100

    Checkerboard

---
+++ Python/Week 1/Day 4/Practice/Checkerboard/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('myindex.html')
+
+@app.route('/<int:x>')
+def checkerboard_rows(x):
+    return render_template('myindex.html', rows=x)
+
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def checkerboard_params(x, y, color1, color2):
+    return render_template('myindex.html', rows=x, cols=y, color1=color1, color2=color2)
+
+if __name__ == '__main__':
+    app.run(debug=True , port=3000)
