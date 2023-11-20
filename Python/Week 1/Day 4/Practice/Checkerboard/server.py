c0a4dbf5f98b9e1c1b53fb2339d9ee4289c0bad5
Samehkhazri <sameh.khazri09@gmail.com>
2023-11-20 18:56:09 +0100

    commit&&push

--- Python/Week 1/Day 4/Practice/Checkerboard/server.py
+++ Python/Week 1/Day 4/Practice/Checkerboard/server.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('myindex.html')
+    return render_template('myindex.html', rows=8, cols=8, color1='#FFFFFF', color2='#000000')
 
 @app.route('/<int:x>')
 def checkerboard_rows(x):
-    return render_template('myindex.html', rows=x)
+    return render_template('myindex.html', rows=x, cols=8, color1='#FFFFFF', color2='#000000')
 
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
 def checkerboard_params(x, y, color1, color2):
     return render_template('myindex.html', rows=x, cols=y, color1=color1, color2=color2)
 
 if __name__ == '__main__':
-    app.run(debug=True , port=3000)
+    app.run(debug=True, port=3000)
